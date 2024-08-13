33dcbe45c8ecf50ef36d769d416921446e6b711e
liuxu <2982359231@qq.com>
2024-08-13 22:46:35 +0800

    vite动态引入静态资源方案2

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,38 +1,32 @@
 <script setup>
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import banner1 from "@/assets/swiper/banner_s3.jpg";
-import banner2 from "@/assets/swiper/chequedeposit_s.jpg";
-import banner3 from "@/assets/swiper/iGTB Mobile Token.jpg";
-import banner4 from "@/assets/swiper/igtb_topbanner.jpg";
-import banner5 from "@/assets/swiper/iGTBTopBanner_SC.jpg";
-import banner6 from "@/assets/swiper/iGTB_banner_api.jpg";
-const slides = [
-  {
-    img: banner1,
-    title: "1",
-  },
-  {
-    img: banner2,
-    title: "2",
-  },
-  {
-    img: banner3,
-    title: "3",
-  },
-  {
-    img: banner4,
-    title: "4",
-  },
-  {
-    img: banner5,
-    title: "5",
-  },
-  {
-    img: banner6,
-    title: "6",
-  },
-];
+  import { Swiper, SwiperSlide } from "swiper/vue";
+  import { Navigation, Pagination, Autoplay } from "swiper/modules";
+  const slides = [
+    {
+      img: new URL("@/assets/swiper/banner_s3.jpg", import.meta.url),
+      title: "1",
+    },
+    {
+      img: new URL("@/assets/swiper/chequedeposit_s.jpg", import.meta.url),
+      title: "2",
+    },
+    {
+      img: new URL("@/assets/swiper/iGTB Mobile Token.jpg", import.meta.url),
+      title: "3",
+    },
+    {
+      img: new URL("@/assets/swiper/igtb_topbanner.jpg", import.meta.url),
+      title: "4",
+    },
+    {
+      img: new URL("@/assets/swiper/iGTBTopBanner_SC.jpg", import.meta.url),
+      title: "5",
+    },
+    {
+      img: new URL("@/assets/swiper/iGTB_banner_api.jpg", import.meta.url),
+      title: "6",
+    },
+  ];
 </script>
 
 <template>
@@ -59,19 +53,19 @@ const slides = [
 </template>
 
 <style lang="less" scoped>
-.main {
-  width: 100%;
-  height: 900px;
-  margin-top: 60px;
-  .swiper-wrapper {
-    width: 100vw;
-    height: 500px;
-    .swiper-slide {
+  .main {
+    width: 100%;
+    height: 900px;
+    margin-top: 60px;
+    .swiper-wrapper {
       width: 100vw;
-      height: 100%;
-      background-repeat: no-repeat;
-      background-size: 100% 100%;
+      height: 500px;
+      .swiper-slide {
+        width: 100vw;
+        height: 100%;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+      }
     }
   }
-}
 </style>
